0f16aebd7ed519874edde413c2cd800b43575f42
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-29 06:41:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+
+document.getElementById("btn").addEventListener("click", async () => {
+	let ip1 = document.getElementById("text");
+	let ip2 = document.getElementById("delay");
+	
+	let text = ip1.value;
+	let delay = parseInt(ip2.value);
+	let output = document.getElementById("output");
+
+	// Function to return a promise with a timeout
+    function delayFunction(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+	// Wait for the delay using async/await
+	  await delayFunction(delay);
+  
+	  // Display the user-provided text after the delay
+	  output.innerText = text;
+})
